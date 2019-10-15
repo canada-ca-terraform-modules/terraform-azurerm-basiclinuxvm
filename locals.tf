8c885e48f3a1205c71fac42c30af9aabbed21ed1
Bernard Maltais <bernard.maltais@canada.ca>
2019-10-15 09:26:48 -0400

    Adopt new naming standard

--- locals.tf
+++ locals.tf
@@ -1,10 +1,11 @@
 # This local is used to create a simple array with a single plan object if a plan is specified as an optional variable
 
 locals {
-  plan = var.plan == null ? [] : [var.plan]
-  ssh_key = var.ssh_key == null ? [] : [var.ssh_key]
+  plan            = var.plan == null ? [] : [var.plan]
+  ssh_key         = var.ssh_key == null ? [] : [var.ssh_key]
   boot_diagnostic = var.boot_diagnostic ? ["1"] : []
   unique          = "${substr(sha1("${data.azurerm_resource_group.resourceGroup.id}"), 0, 8)}"
   fixname         = "${replace("${var.name}", "-", "")}"
-  storageName     = "${lower("${local.fixname}diag${local.unique}")}"
+  fixname2        = "${replace("${var.name}", "_", "")}"
+  storageName     = "${lower("${local.fixname2}diag${local.unique}")}"
 }
